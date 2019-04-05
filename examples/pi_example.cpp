7fa712ccd2b5cb2c381b23e39d26ee034f2fb6c5
Jacob Killelea <Jkillelea@protonmail.ch>
2019-04-04 23:22:12 -0600

    misc

--- examples/pi_example.cpp
+++ examples/pi_example.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
     while (true) {
         for (int chan = 0; chan < 8; chan++) {
             uint16_t val = adc.analogRead(chan);
-            printf("%6x ", val);
+            printf("%6d ", val);
         }
         printf("\n");
         delay(500);
