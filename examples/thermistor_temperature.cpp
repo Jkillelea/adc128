a34f84d2ada664c0543b4bc2afb2d5b85363b588
Jacob Killelea <Jkillelea@protonmail.ch>
2019-04-16 13:41:37 -0600

    thermistor readings

--- examples/thermistor_temperature.cpp
+++ examples/thermistor_temperature.cpp
@@ -4,6 +4,8 @@
 #include <wiringPi.h>
 #include "adc128.h"
 
+double calculate_temperature(uint16_t adc_reading);
+
 int main(int argc, char *argv[]) {
     usleep(100000); // wait more than 33ms on poweron
     ADC128 adc(0x1D);
@@ -16,7 +18,7 @@ int main(int argc, char *argv[]) {
     while (true) {
         for (uint8_t chan = 0; chan < 8; chan++) {
             uint16_t val = adc.analogRead(chan);
-            printf("%6d ", calculate_temperature(val));
+            printf("%6f ", calculate_temperature(val) - 273.15);
         }
         printf("\n");
         delay(500);
